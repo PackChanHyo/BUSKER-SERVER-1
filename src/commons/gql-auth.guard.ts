72df6436e3371f961c2eee3f33a69a60dbd49aa9
PackChanHyo <cksgy1510@gmail.com>
2023-04-05 14:53:31 +0900

    authService 변경

--- src/commons/gql-auth.guard.ts
+++ src/commons/gql-auth.guard.ts
@@ -3,26 +3,22 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 export class GqlAuthAccessGuard extends AuthGuard('access') {
-  async getRequest(context: ExecutionContext) {
+  getRequest(context: ExecutionContext) {
     const gqlContext = GqlExecutionContext.create(context);
-    return await gqlContext.getContext().req;
+    return gqlContext.getContext().req;
   }
 }
 
 export class GqlAuthRefreshGuard extends AuthGuard('refresh') {
-  async getRequest(context: ExecutionContext) {
+  getRequest(context: ExecutionContext) {
     const gqlContext = GqlExecutionContext.create(context);
-    return await gqlContext.getContext().req;
+    return gqlContext.getContext().req;
   }
 
-  handleRequest(err, user, info) {
-    try {
-      if (err || !user) {
-        throw err || info;
-      }
-      return user;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // handleRequest(err, user, info) {
+  //   if (err || !user) {
+  //     throw err || info;
+  //   }
+  //   return user;
+  // }
 }
