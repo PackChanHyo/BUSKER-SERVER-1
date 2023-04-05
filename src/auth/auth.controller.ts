2508ec980564ef2afdca80ea59d7e6cdfc5d2b23
PackChanHyo <cksgy1510@gmail.com>
2023-04-05 15:10:17 +0900

    authService 변경

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,33 +10,31 @@ interface IOAuthUser {
 
 @Controller()
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService, //
-  ) {}
-
-  @Get('/login/google')
-  @UseGuards(AuthGuard('google'))
-  async loginGoogle(
-    @Req() req: Request & IOAuthUser, //
-    @Res() res: Response,
-  ) {
-    this.authService.buskerSocialLogin({ req, res });
-  }
-
-  @Get('/login/kakao')
-  @UseGuards(AuthGuard('kakao'))
-  async loginKakao(
-    @Req() req: Request & IOAuthUser, //
-    @Res() res: Response,
-  ) {
-    this.authService.buskerSocialLogin({ req, res });
-  }
-  @Get('/login/facebook')
-  @UseGuards(AuthGuard('facebook'))
-  async loginFacebook(
-    @Req() req: Request & IOAuthUser, //
-    @Res() res: Response,
-  ) {
-    this.authService.buskerSocialLogin({ req, res });
-  }
+  // constructor(
+  //   private readonly authService: AuthService, //
+  // ) {}
+  // @Get('/login/google')
+  // @UseGuards(AuthGuard('google'))
+  // async loginGoogle(
+  //   @Req() req: Request & IOAuthUser, //
+  //   @Res() res: Response,
+  // ) {
+  //   this.authService.buskerSocialLogin({ req, res });
+  // }
+  // @Get('/login/kakao')
+  // @UseGuards(AuthGuard('kakao'))
+  // async loginKakao(
+  //   @Req() req: Request & IOAuthUser, //
+  //   @Res() res: Response,
+  // ) {
+  //   this.authService.buskerSocialLogin({ req, res });
+  // }
+  // @Get('/login/facebook')
+  // @UseGuards(AuthGuard('facebook'))
+  // async loginFacebook(
+  //   @Req() req: Request & IOAuthUser, //
+  //   @Res() res: Response,
+  // ) {
+  //   this.authService.buskerSocialLogin({ req, res });
+  // }
 }
